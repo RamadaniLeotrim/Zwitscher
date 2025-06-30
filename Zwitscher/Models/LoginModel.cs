3e8c48ece0bb81d27b062749303c2823050ae622
130 <81311415+Leotrim130@users.noreply.github.com>
2025-06-30 09:36:29 +0200

    added LoginModel.cs

---
+++ Zwitscher/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zwitscher.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Benutzername ist erforderlich")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Passwort ist erforderlich")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
