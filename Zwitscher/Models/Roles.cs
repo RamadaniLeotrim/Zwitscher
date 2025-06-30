d87eb7c98816e4ac9b2d987c9b161a27a935d005
130 <81311415+Leotrim130@users.noreply.github.com>
2025-06-30 11:36:12 +0200

    Roles.cs & User.cs Models created

---
+++ Zwitscher/Models/Roles.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Zwitscher.Models
+{
+    
+    public enum Roles
+    {
+        [Description("Administrator")]
+        Admin,
+        [Description("Benutzer")]
+        User
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            try
+            {
+                var field = value.GetType().GetField(value.ToString());
+                var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                return attribute?.Description ?? value.ToString();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it as needed
+                Console.WriteLine($"Error retrieving description: {ex.Message}");
+
+                return value.ToString();
+            }
+
+
+        }
+    }
+}
---
+++ Zwitscher/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zwitscher.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
